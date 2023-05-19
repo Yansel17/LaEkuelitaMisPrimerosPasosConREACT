cccc14c8160d3542d5789e9f8f509b3e9714d716
Yansel17 <20211594@itla.edu.do>
2023-05-19 10:02:09 -0400

    Error ma raro ese

---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import "./Form.css"
+
+const Form = () => {
+    return <section>Formularios
+        <Form>
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+        </Form>
+    </section>
+}
+
+export default Form
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+function Header() {
+    return <header>
+        <img src="/img/img-org.png" alt ="img-org"/>
+    </header>
+}
+
+export default Header
